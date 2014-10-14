e80b5bfd828a19314f6a38044ef8a1611095638d
sova17 <Prometey15@yandex.ru>
2014-10-14 14:25:56 +0400

    Big part of abstract classes added, ship classes are not full completed

--- game_scripts/ShipController.cs
+++ game_scripts/ShipController.cs
@@ -25,7 +25,7 @@ using System.Threading.Tasks;
 //}
 
 namespace templates {
-	class TBaseMap {
+	abstract class TBaseMap {
 	}
 	abstract class TBaseMapController {
 		public abstract void CalculateAvailableArea(TBaseMap map);
@@ -52,22 +52,91 @@ namespace templates {
 		public abstract void Execute(TBaseShipController shipController, Object obj);
 	}
 	abstract class TBaseShipAction : TBaseAction {
-		public void Execute(TBaseShipController shipController, Object obj) {
+		public override void Execute(TBaseShipController shipController, Object obj) {
 			Execute(shipController, (TBaseShip)obj);
 		}
 		public abstract void Execute(TBaseShipController shipController, TBaseShip ship);
 	}
-	abstract class TBaseCellAction :TBaseAction {
-		public void Execute(TBaseShipController shipController, Object obj) {
+	abstract class TBaseCellAction : TBaseAction {
+		public override void Execute(TBaseShipController shipController, Object obj) {
 			Execute(shipController, (TBaseCell)obj);
 		}
 		public abstract void Execute(TBaseShipController shipController, TBaseCell cell);
 	}
-	class TBaseCell {
+	abstract class TBaseCell {
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Depth { get; set; }
+		//public TSpecificators BonusSpecificators { get; set; }
+	
+	}
+	abstract class TBaseShip {
+		public String Name { get; set; }
+		public String ClassName { get; set; }
+		public int CreationYear { get; set; }
+		public TNation CreationNation { get; set; }
+		public TNation PlayingNation { get; set; }
+		public int Level { get; set; }
+		public int Experience { get; set; }
+		public int RequiredCommandLevel { get; set; }
+		public int Width { get; private set; }
+		public int Length { get; private set; }
+		public int Weight { get; private set; }
+		//
+		// Пушки и груз!!!!
+		//
+		public int Observation { get; private set; }
+		public int Subtlety { get; private set; }
+		public PartsScheme<int> Armour { get; private set; }
+		public PartsScheme<int> HitPoints { get; private set; }
+		public PartsScheme<int> NumberOfGuns { get; private set; }
+		public int Sharpshooting { get; private set; }
+		public int Speed { get; private set; }
+		public TDimesions Dimensions { get; private set; }
+		public int Luck { get; set; }
+		public int Moral { get; set; }
+		public int Initiative { get; set; }
+
+		public class PartsScheme<T> {
+			public T HullLeft { get; set; }
+			public T HullRight { get; set; }
+			public T HullTail { get; set; }
+			public T HullHead { get; set; }
+			public T Deck { get; set; }
+			public T Mast { get; set; }
+		}
+		public class TDimesions {
+			public int Draft { get; set; }
+			public int BearingCapacity { get; set; }
+		}
+		public enum ShipClass {
+			BattleShip = 1,
+			Frigate,
+			Carvette,
+			Galley,
+			Trader,
+			Sloop,
+			FishingBoat
+		}	
+		public enum TNation {
+			England,
+			Spain,
+			Portugal,
+			France,
+			Brothers
+		}
 
 	}
-	class TBaseShip {
-
+	abstract class Cannon {
+		public int Weight { get; set; }
+		public int MaxCannonballWeight { get; set; }
+		public TBaseShip.PartsScheme<int> Sharpshooting { get; set; }
+	}
+	abstract class CannonBall {
+		public int Weight { get; set; }
+		public int AirResistence { get; set; }
+		public TBaseShip.PartsScheme<int> Damage { get; set; }
+		public TBaseShip.PartsScheme<int> Sharpshooting { get; set; }
 	}
 	/// <summary>
 	/// for initializing TShipController and abstraction between inside and outside representation
