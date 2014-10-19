bcad6bb42c2c53b2633612be57fb64fe18a06ab5
sova17 <Prometey15@yandex.ru>
2014-10-19 20:52:57 +0400

    Parameters & parameters controller  interaction defined

--- game_scripts/BattleParameters.cs
+++ game_scripts/BattleParameters.cs
@@ -5,78 +5,100 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace game_scripts {
-	class TBattleParameters {
-		public Int32 Observation { get; protected set; }
-		public Int32 Subtlety { get; protected set; }
-		public ShipPartsScheme<Int32> Armour { get; protected set; }
-		public ShipPartsScheme<Int32> HitPoInt32s { get; protected set; }
-		public ShipPartsScheme<Int32> NumberOfGuns { get; protected set; }
-		public Int32 Sharpshooting { get; protected set; }
-		public Int32 Speed { get; protected set; }
-		public Int32 Draft { get; protected set; }
-		public Int32 Weight { get; protected set; }
-		public Int32 Luck { get; protected set; }
-		public Int32 Moral { get; protected set; }
-		public Int32 Initiative { get; protected set; }
+	struct TParameters {
+		public Int32 Observation { get; set; }
+		public Int32 Subtlety { get; set; }
+		public ShipPartsScheme<Int32> Armour { get; set; }
+		public ShipPartsScheme<Int32> HitPoints { get; set; }
+		public ShipPartsScheme<Int32> NumberOfGuns { get; set; }
+		public Int32 Sharpshooting { get; set; }
+		public Int32 Speed { get; set; }
+		public Int32 Draft { get; set; }
+		public Int32 Weight { get; set; }
+		public Int32 Luck { get; set; }
+		public Int32 Moral { get; set; }
+		public Int32 Initiative { get; set; }
 	}
 	struct ShipPartsScheme<T> {
-		public T HullLeft { get; private set; }
-		public T HullRight { get; private set; }
-		public T HullTail { get; private set; }
-		public T HullHead { get; private set; }
-		public T Deck { get; private set; }
-		public T Mast { get; private set; }
+		public T HullLeft { get; set; }
+		public T HullRight { get; set; }
+		public T HullTail { get; set; }
+		public T HullHead { get; set; }
+		public T Deck { get; set; }
+		public T Mast { get; set; }
 	}
-	abstract class TVariableBattleParameters : TBattleParameters {
-		public void AddObservation(Int32 addition) {
+	abstract class TBaseParametersController {
+		public virtual TParameters Parameters { get; set; }
+		public abstract void AddObservation(Int32 addition);
+		public abstract void AddSubtlety(Int32 addition);
+		public abstract void AddArmour(ShipPartsScheme<Int32> addition);
+		public abstract void AddHitPoints(ShipPartsScheme<Int32> addition);
+		public abstract void AddNumberOfGuns(ShipPartsScheme<Int32> addition);
+		public abstract void AddSharpshooting(Int32 addition);
+		public abstract void AddSpeed(Int32 addition);
+		public abstract void AddDraft(Int32 addition);
+		public abstract void AddWeight(Int32 addition);
+		public abstract void AddLuck(Int32 addition);
+		public abstract void AddMoral(Int32 addition);
+		public abstract void AddInitiative(Int32 addition);
+	}
+	class TParametersController : TBaseParametersController { 
+		private TParameters _parameters;
+		public new TParameters Parameters {
+			get {
+				return _parameters;
+			}
+			set {
+				//!!!!!!!!!!!! NOT RIGHT !!!!!!!!!!
+				_parameters = value;
+			}
+		}
+		public override void AddObservation(Int32 addition) {
+			this._parameters.Observation += addition;
+		}
+		public override void AddSubtlety(Int32 addition) {
+			this._parameters.Subtlety += addition;
+		}
+		public override void AddArmour(ShipPartsScheme<Int32> addition) {
+			this._parameters.Armour += addition;
+		}
+		public override void AddHitPoints(ShipPartsScheme<Int32> addition) {
+			this._parameters.HitPoints += addition;
+		}
+		public override void AddNumberOfGuns(ShipPartsScheme<int> addition) {
+			this._parameters.NumberOfGuns += addition;
+		}
+		public override void AddSharpshooting(int addition) {
+			this._parameters.Sharpshooting += addition;
+		}
+		public override void AddSpeed(int addition) {
+			this._parameters.Speed += addition;
+		}
+		public override void AddDraft(int addition) {
+			this._parameters.Draft += addition;
+		}
+		public override void AddWeight(int addition) {
 			throw new NotImplementedException();
 		}
-		public void AddSubtlety(Int32 addition) {
+		public override void AddLuck(int addition) {
 			throw new NotImplementedException();
 		}
-		public void AddArmour(ShipPartsScheme<Int32> addition) {
+		public override void AddMoral(int addition) {
 			throw new NotImplementedException();
 		}
-		public void AddHitPoInt32s(ShipPartsScheme<Int32> addition) {
+		public override void AddInitiative(int addition) {
 			throw new NotImplementedException();
 		}
-		public void AddNumberOfGuns(ShipPartsScheme<Int32> addition) {
-			throw new NotImplementedException();
-		}
-		public void AddSharpshooting(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddSpeed(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddDraft(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddWeight(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddLuck(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddMoral(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddInitiative(Int32 addition) {
-			throw new NotImplementedException();
-		}
-		public void AddBattleParameters(TBattleParameters param) {
-			throw new NotImplementedException();
-		}
-	}	
-	abstract class TBaseBalancingBattleVariableParameters : TVariableBattleParameters {
+	}
+	abstract class TBaseBalancingBattleVariableParameters {
 		public abstract void UpdateParameters();
 	}
-	class BindedVariableBattleParameters : TVariableBattleParameters {
-		private TVariableBattleParameters targer;
+	class BindedVariableBattleParameters {
+		private TParameters targer;
 
 	}
 	class DoubleVariableBattleParameters {
-		public TVariableBattleParameters Current { get; set; }
+		public TParameters Current { get; set; }
 		public BindedVariableBattleParameters Full {get; private set;}
 	}
 }
--- game_scripts/Map.cs
+++ game_scripts/Map.cs
@@ -9,7 +9,7 @@ namespace game_scripts {
 		public Int32 X { get; set; }
 		public Int32 Y { get; set; }
 		public Int32 Depth { get; set; }
-		public TBattleParameters BonusSpecificators { get; set; }
+		public TParameters BonusSpecificators { get; set; }
 		// Here will be yet some parameters, for example about weather in the cell, etc
 	}
 	abstract class TBaseMap {
